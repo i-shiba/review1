0953f3aab525b360a3c6601ca461f6c1d8315c31
i-shiba <sbtior@yahoo.co.jp>
2012-08-09 20:24:02 +0900

    Parsonadd

---
+++ src/Parson.java
@@ -0,0 +1,32 @@
+public class Parson{
+	static String uji = "氏";
+	static String na  = "名";
+	String firstName  = null;
+	String lastName   = null;
+	public getFirstName(){
+		return this.firstName;
+	}
+	public setFirstName(String name){
+		this.firstName = name;
+	}
+	public getlastName(){
+		return this.lastName;
+	}
+	public setlastName(String name){
+		this.lastName = name;
+	}
+	public showName(){
+		System.out.print(Parson.uji + ":" + this.lastName " " + Parson.na + ":" + this.firstName);
+	}
+	public static void main String args[]{
+		public void setlastName("渡辺");
+		public void setFirstName("高志");
+		public void showName();
+		public void setlastName("和田");
+		public void setFirstName("充史");
+		public void showName();
+		public void setlastName("渡辺");
+		public void setFirstName("高志");
+		public void showName();
+	}
+}
